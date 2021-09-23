c87a207242d0dd70e2a3852a15e81265510168df
Corné van Rooyen <glologins@glolighting.co.za>
2021-09-23 14:14:35 +0200

    Fixed whitespaces in map.js

--- view/frontend/web/js/view/map.js
+++ view/frontend/web/js/view/map.js
@@ -12,25 +12,25 @@ define([
 ], function ($) {
     'use strict';
     return {
-        initMap: function() {
+        initMap: function () {
             var self = this;
             var myLatLng = {
-                lat: window.checkoutConfig.shipping.select_store.lat, 
+                lat: window.checkoutConfig.shipping.select_store.lat,
                 lng: window.checkoutConfig.shipping.select_store.lng
             };
             var map = new google.maps.Map(document.getElementById('map-canvas'), {
-              zoom: window.checkoutConfig.shipping.select_store.zoom,
-              center: myLatLng
+                zoom: window.checkoutConfig.shipping.select_store.zoom,
+                center: myLatLng
             });
 
             var stores = $.parseJSON(window.checkoutConfig.shipping.select_store.stores);
             var infoWindow = new google.maps.InfoWindow();
 
-            $.each(stores, function(index, obj) {
-                if($.type( obj ) !== "array"){
+            $.each(stores, function (index, obj) {
+                if ($.type(obj) !== "array") {
                     return;
                 }
-                $.each(obj, function(key, store) {
+                $.each(obj, function (key, store) {
                     var latitude = parseFloat(store.latitude),
                         longitude = parseFloat(store.longitude),
                         latLng = new google.maps.LatLng(latitude, longitude),
@@ -40,11 +40,11 @@ define([
                             title: store.name
                         });
 
-                    (function(marker, store) {
-                        google.maps.event.addListener(marker, 'click', function(e) {
-                            infoWindow.setContent('<h3>'+ store.name + '</h3><br /><br /><strong>Address: </strong>' + store.address + '<br /><br />'
-                                + store.working_time.replace(/(?:\r\n|\r|\n)/g, '<br />') +'<br /><br /><button data-id="'
-                                + store.store_id + '" data-name="'+ store.name +'" class="apply-store">Pick Up Here!</button></div>');
+                    (function (marker, store) {
+                        google.maps.event.addListener(marker, 'click', function (e) {
+                            infoWindow.setContent('<h3>' + store.name + '</h3><br /><br /><strong>Address: </strong>' + store.address + '<br /><br />'
+                                + store.working_time.replace(/(?:\r\n|\r|\n)/g, '<br />') + '<br /><br /><button data-id="'
+                                + store.store_id + '" data-name="' + store.name + '" class="apply-store">Pick Up Here!</button></div>');
                             infoWindow.open(map, marker);
                         });
                     })(marker, store);
